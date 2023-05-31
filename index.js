3b5af522e8c67e45f609f2fe7d171c3ac9e08e8a
Priyam Das <domidas01@gmail.com>
2023-05-31 04:40:25 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body:JSON.stringify({
+            "name": userName,
+            "email": userEmail
+        })
+    })
+    .then(res => res.json())
+    .then(data=>{
+        document.querySelector("body").innerHTML = data.id; 
+    })
+    .catch(error=>{
+        document.querySelector("body").innerHTML = error.message;
+    });
+}
+// submitData("tutu", "gmail")
